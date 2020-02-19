81a292ac8230f36db0a20f2d37761d31d6ecfc7e
Raghav Verma <42772063+WolverineXR@users.noreply.github.com>
2020-02-19 21:10:27 +0530

    Added WayTooLongWord.cpp

---
+++ 1_Codeforces_Rating_Less_than_1300/WayTooLongWords.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    while(n--){
+        string s;
+        cin>>s;
+        if(s.size() <= 10)
+            cout<<s<<endl;
+        else
+            cout<<(s[0] + to_string(s.size()-2) + s[s.size() - 1])<<endl;
+    }
+    return 0;
+}
