133da6c19b2790bcdb8fa928a34b22c1a6b14509
Raghav Verma <42772063+WolverineXR@users.noreply.github.com>
2020-03-02 16:18:16 +0530

    Added LittleElephantAndFunction.cpp

---
+++ 1_Codeforces_Rating_Less_than_1300/LittleElephantAndFunction.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int x;
+    cin>>x;
+    cout<<x;
+    for(int i=1;i<x;i++)
+        cout<<" "<<i;
+    cout<<"\n";
+    return 0;
+}
