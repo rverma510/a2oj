1c6b5a6ab0a731776cb9c5489f3a0d7c9e03161a
Raghav Verma <42772063+WolverineXR@users.noreply.github.com>
2020-03-09 17:37:31 +0530

    Added Blackjack.cpp

---
+++ 2_Codeforces_Rating_Less_than_1400/Blackjack.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    if(n - 10 < 0)
+        cout<<0<<endl;
+    else if(n - 10 == 0)
+        cout<<0<<endl;
+    else if(n - 10 == 1)
+        cout<<4<<endl;
+    else if(n - 10 < 10 )
+        cout<<4<<endl;
+    else if(n - 10 == 10)
+        cout<<15<<endl;
+    else if(n - 10 == 11)
+        cout<<4<<endl;
+    else
+        cout<<0<<endl;
+    return 0;
+}
