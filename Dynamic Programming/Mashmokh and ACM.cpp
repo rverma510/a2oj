e44ea83dd38b9ba69d5cdf5956792c60808ab9a3
rverma510 <raghavverma4091@gmail.com>
2020-10-10 23:11:13 +0530

    Modified Mashmokh and ACM.cpp

--- Dynamic Programming/Mashmokh and ACM.cpp
+++ Dynamic Programming/Mashmokh and ACM.cpp
@@ -15,11 +15,28 @@ ll solve(ll prev, ll n, ll k, vector<vector<ll>> &mem) {
     }
     return mem[prev][k] = ans % mod;
 }
+ll solveDP(ll n, ll k) {
+    vector<vector<ll>> dp(k + 1, vector<ll>(n + 1, 0));
+    for (ll i = 1; i <= n; i++) dp[1][i] = 1;
+    for (ll i = 2; i <= k; i++) {
+        for (ll j = 1; j <= n; j++) {
+            for (ll l = j; l <= n; l += j) {
+                dp[i][l] = (dp[i][l] + dp[i - 1][j]) % mod;
+            }
+        }
+    }
+    ll ans = 0;
+    for (ll i = 1; i <= n; i++) {
+        ans = (ans + dp[k][i]) % mod;
+    }
+    return ans;
+}
 int main() {
     ll n, k;
     cin >> n >> k;
-    vector<vector<ll>> mem(n + 1, vector<ll>(k + 1, -1));
-    ll ans = solve(1, n, k, mem);
+    // vector<vector<ll>> mem(n + 1, vector<ll>(k + 1, -1));
+    // ll ans = solve(1, n, k, mem);
+    ll ans = solveDP(n, k);
     cout << ans << "\n";
     return 0;
 }
